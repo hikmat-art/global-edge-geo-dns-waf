2e9b37ed07e03414b622ab735d5afeb11692cc9c
hikmat-art <itcombat97@gmail.com>
2026-01-02 10:09:19 +0500

    Update main.tf

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -1,36 +1,6 @@
-resource "aws_route53_zone" "root" {
-  name = var.root_domain
-}
+# Stub DNS module for demo/demo mode
+# No hosted zone or records created
 
-# Example geolocation records
-resource "aws_route53_record" "us_record" {
-  zone_id = aws_route53_zone.root.zone_id
-  name    = var.root_domain
-  type    = "A"
-
-  geo_location {
-    country = "US"
-  }
-
-  alias {
-    name                   = var.us_endpoint
-    zone_id                = var.us_zone_id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_route53_record" "ap_record" {
-  zone_id = aws_route53_zone.root.zone_id
-  name    = var.root_domain
-  type    = "A"
-
-  geo_location {
-    country = "IN"
-  }
-
-  alias {
-    name                   = var.ap_endpoint
-    zone_id                = var.ap_zone_id
-    evaluate_target_health = true
-  }
+output "zone_id" {
+  value = "dummy-zone-id"
 }
